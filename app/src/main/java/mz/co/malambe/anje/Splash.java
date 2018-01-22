574a483bce06373a2f7ae0bb95fb6e7c0c6e0e51
machiza <machizaarmando@gmail.com>
2018-01-22 03:13:25 +0200

    Initial commit

---
+++ app/src/main/java/mz/co/malambe/anje/Splash.java
@@ -0,0 +1,13 @@
+package mz.co.malambe.anje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
